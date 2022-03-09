977b4c66a68f52f1f4b76288bc25496510b4b66a
Alison <alisonserafim@gmail.com>
2022-03-09 18:30:41 -0300

    Add Bootstrap5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 createApp(App).use(router).mount('#app')
