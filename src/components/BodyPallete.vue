131a22f057a9f4dedf295ab810da579b43297cfa
Alison <alisonserafim@gmail.com>
2022-03-10 16:12:31 -0300

    Add Body Pallete Component

---
+++ src/components/BodyPallete.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-body-pallete">
+      <ul class="ul-body-pallete-list">
+          <li v-for="(pallete, index) in body_pallete_list" :key="index" class="li-body-pallete">
+              <span :tooltip="pallete.name">{{pallete.id}}</span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BodyPallet',
+    data() {
+        return {
+            body_pallete_list: [
+                {
+                    id: 0,
+                    name: 'regular'
+                },
+                {
+                    id: 1,
+                    name: 'blue'
+                },
+                {
+                    id: 2,
+                    name: 'red'
+                },
+                {
+                    id: 3,
+                    name: 'green'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.ul-body-pallete-list {
+    list-style: none;
+}
+.li-body-pallete {
+    float: left;
+    border: solid 1px;
+    border-color: black;
+    padding: 5px;
+    margin: 5px;
+}
+</style>
