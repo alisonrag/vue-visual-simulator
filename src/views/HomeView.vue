f485e89cde3389094c9b8e32d76e3b4a53358661
Alison <alisonserafim@gmail.com>
2022-03-11 18:14:32 -0300

    Update Homeview Component
    load all components to test in production

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,43 @@
 <template>
   <div class="home">
+    <GenderList/>
+    <HairColorList/>
+    <HeadList />
+    <BodyPallete />
+    <JobList/>
+    <ItemListHeadTop />
+    <ItemListHeadMid />
+    <ItemListHeadBottom />
+    <ItemListGarmet />
+    <Character />
   </div>
 </template>
 
 <script>
+import GenderList from "@/components/GenderList.vue"
+import HairColorList from "@/components/HairColorList.vue"
+import HeadList from "@/components/HeadList.vue"
+import BodyPallete from "@/components/BodyPallete.vue"
+import JobList from "@/components/JobList.vue"
+import ItemListHeadTop from "@/components/ItemListHeadTop.vue"
+import ItemListHeadMid from "@/components/ItemListHeadMid.vue"
+import ItemListGarmet from "@/components/ItemListGarmet.vue"
+import ItemListHeadBottom from "@/components/ItemListHeadBottom.vue"
+import Character from "@/components/Character.vue"
+
 export default {
+  components: {
+    GenderList,
+    HairColorList,
+    HeadList,
+    BodyPallete,
+    JobList,
+    ItemListHeadTop,
+    ItemListHeadMid,
+    ItemListHeadBottom,
+    ItemListGarmet,
+    Character
+  },
   mounted() {
     document.title = 'Ragnarok Online Visual Simulator'
   }
