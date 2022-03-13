8ad940066fea7b8dd93bd5a58a831d5e5e6acdc6
Alison <alisonserafim@gmail.com>
2022-03-13 13:29:31 -0300

    fix wrong href on build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/vue-visual-simulator/'
+    publicPath: '/'
 })
