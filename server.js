d73c502fb5e81dd23f271d7411cd2e324d84b9be
Alison <alisonserafim@gmail.com>
2022-03-13 12:50:38 -0300

    heroku test

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
