642bd1cf2e0030e5eb339ff298e809ff4645f9cc
Alison <alisonserafim@gmail.com>
2022-03-22 14:33:36 -0300

    add vuex

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
