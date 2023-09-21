c7d6ad8062e7660221ffd287ea36ceb0c0cc41ea
Alison <alisonserafim@gmail.com>
2023-09-21 17:18:03 -0300

    feat: add basic support to items equipped in many location

--- src/components/character/ItemListHeadBottom.vue
+++ src/components/character/ItemListHeadBottom.vue
@@ -1,56 +1,72 @@
 <template>
   <ul class="ul-item-list">
-    <li
-      v-for="(item, index) in items"
-      v-show="item.name.toLowerCase().includes(item_filter.toLowerCase())"
-      class="li-item"
-      :key="item.id"
-    >
-      <img
-        v-lazy="{ src: 'https://static.divine-pride.net/images/items/item/' +
+    <li v-for="(item, index) in items" v-show="item.name.toLowerCase().includes(item_filter.toLowerCase())"
+      class="li-item" :key="item.id">
+      <img v-lazy="{
+        src: 'https://static.divine-pride.net/images/items/item/' +
           item.id +
-          '.png', delay: 500 }
-        "
-        class="img-item"
-        :class="{
-          'item-selected': parseInt(item.id) == $store.state.headgear_bottom_id,
-          'item-disabled': parseInt(item.viewID) == 0,
-        }"
-        :viewID="item.viewID"
-        :id="item.id"
-        data-bs-toggle="tooltip"
-        data-bs-placement="top"
-        :title="item.name"
-        :name="item.name"
-        @click="clickItem($event)"
-      />
+          '.png', delay: 500
+      }
+        " class="img-item" :class="{
+    'item-selected': parseInt(item.id) == $store.state.headgear_bottom_id,
+    'item-disabled': parseInt(item.viewID) == 0,
+  }" :viewID="item.viewID" :id="item.id" data-bs-toggle="tooltip" data-bs-placement="top" :title="item.name"
+        :name="item.name" :top="item.top" :mid="item.mid" :bot="item.bot" :garment="item.garment"
+        @click="clickItem($event)" />
     </li>
   </ul>
 </template>
 
 <script>
 import { mapMutations } from "vuex";
-import itemsJson from './item_db/bottom.json';
+import itemsJson from './item_db/items.json';
 
 export default {
   name: "ItemListHeadBottom",
   props: ["item_filter"],
   data() {
     return {
-      items: itemsJson,
+      items: itemsJson.filter(function (item) {
+        return item.bot == true;
+      }),
       active: false,
     };
   },
   methods: {
     ...mapMutations([
+      "SAVE_HEADGEAR_TOP",
+      "SAVE_HEADGEAR_TOP_ID",
+      "SAVE_HEADGEAR_TOP_NAME",
+      "SAVE_HEADGEAR_MID",
+      "SAVE_HEADGEAR_MID_ID",
+      "SAVE_HEADGEAR_MID_NAME",
       "SAVE_HEADGEAR_BOTTOM",
       "SAVE_HEADGEAR_BOTTOM_ID",
       "SAVE_HEADGEAR_BOTTOM_NAME",
-    ]),
+      "SAVE_GARMENT",
+      "SAVE_GARMENT_ID",
+      "SAVE_GARMENT_NAME"]),
     clickItem: function (event) {
-      this.SAVE_HEADGEAR_BOTTOM(parseInt(event.target.getAttribute("viewID")));
-      this.SAVE_HEADGEAR_BOTTOM_ID(parseInt(event.target.getAttribute("id")));
-      this.SAVE_HEADGEAR_BOTTOM_NAME(event.target.getAttribute("name"));
+      if (event.target.getAttribute("top")) {
+        this.SAVE_HEADGEAR_TOP(parseInt(event.target.getAttribute("viewID")));
+        this.SAVE_HEADGEAR_TOP_ID(parseInt(event.target.getAttribute("id")));
+        this.SAVE_HEADGEAR_TOP_NAME(event.target.getAttribute("name"));
+      }
+      if (event.target.getAttribute("mid")) {
+        this.SAVE_HEADGEAR_MID(parseInt(event.target.getAttribute("viewID")));
+        this.SAVE_HEADGEAR_MID_ID(parseInt(event.target.getAttribute("id")));
+        this.SAVE_HEADGEAR_MID_NAME(event.target.getAttribute("name"));
+      }
+      if (event.target.getAttribute("bot")) {
+        this.SAVE_HEADGEAR_BOTTOM(parseInt(event.target.getAttribute("viewID")));
+        this.SAVE_HEADGEAR_BOTTOM_ID(parseInt(event.target.getAttribute("id")));
+        this.SAVE_HEADGEAR_BOTTOM_NAME(event.target.getAttribute("name"));
+      }
+      if (event.target.getAttribute("garment")) {
+        this.SAVE_GARMENT(parseInt(event.target.getAttribute("viewID")));
+        this.SAVE_GARMENT_ID(parseInt(event.target.getAttribute("id")));
+        this.SAVE_GARMENT_NAME(event.target.getAttribute("name"));
+      }
     },
   },
 };
